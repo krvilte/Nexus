61d99ffb27940e07813786d8bc23ef962a11941b
Vishal Kumar Sah <thevishalkumar25@gmail.com>
2025-01-25 23:43:07 +0530

    added required middlewares

--- src/app.js
+++ src/app.js
@@ -1,4 +1,13 @@
 import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
 const app = express();
+
+app.use(cors({ origin: process.env.CORS_ORIGIN, credentials: true }));
+app.use(cookieParser());
+app.use(express.json({ limit: "40kb" }));
+app.use(express.urlencoded({ limit: "40kb", extended: false }));
+app.use(express.static("public"));
 
 export default app;
