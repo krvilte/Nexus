bef54897dcd109b7af13d07055ec1cba4512565e
Vishal Kumar Sah <thevishalkumar25@gmail.com>
2025-01-26 22:52:37 +0530

    added user and video model

---
+++ src/models/user.model.js
@@ -0,0 +1,52 @@
+import { Schema, model } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    fullName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+
+    avatar: {
+      type: String,
+      required: true,
+    },
+    converImage: {
+      type: String,
+    },
+    watchHistory: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Video",
+      },
+    ],
+    refreshToken: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = model("User", userSchema);
+export default User;
---
+++ src/models/video.model.js
@@ -0,0 +1,39 @@
+import { Schema, model } from "mongoose";
+
+const videoSchema = new Schema(
+  {
+    videoFile: {
+      type: String,
+      required: true,
+    },
+    thumbnail: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    duration: {
+      type: Number,
+      required: true,
+    },
+    views: {
+      type: Number,
+      default: 0,
+    },
+    videoOwner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+export const Video = model("Video", videoSchema);
