fea1f96f5f6cb4b3a38cbb9f57c2c75be7ffd4a0
Vishal Kumar Sah <thevishalkumar25@gmail.com>
2025-01-28 20:42:19 +0530

    configured controllers and routers

--- src/app.js
+++ src/app.js
@@ -10,4 +10,10 @@ app.use(express.json({ limit: "40kb" }));
 app.use(express.urlencoded({ limit: "40kb", extended: false }));
 app.use(express.static("public"));
 
+//Import Routes
+import userRouter from "./routes/user.routes.js";
+
+// Router declaration
+app.use("/api/v1/users", userRouter);
+
 export default app;
