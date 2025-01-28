b10439f502fddf491f751cf7f241a784323da59b
Vishal Kumar Sah <thevishalkumar25@gmail.com>
2025-01-29 00:42:50 +0530

    Added register user controller

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,7 +1,53 @@
 import asyncHandler from "../utils/asyncHandler.js";
+import ApiError from "../utils/apiError.js";
+import ApiResponse from "../utils/apiResponse.js";
+import cloudinaryUpload from "../services/cloudinary.service.js";
+import User from "../models/user.model.js";
 
 export const registerUser = asyncHandler(async (req, res) => {
-  res.status(200).json({
-    message: "Ok",
+  const { fullName, username, email, password } = req.body;
+  const avatarFilePath = req.files?.avatar[0]?.path;
+  const coverImageFilePath = req.files?.coverImage[0]?.path;
+
+  // Validate user inputs
+  if (!fullName && !username && !email && !password)
+    throw new ApiError(400, "All fields are required");
+
+  // Check for existing user
+  const existingUser = await User.findOne({ $or: [{ username }, { email }] });
+  if (existingUser)
+    throw new ApiError(409, "User with username or email already exists");
+
+  // validate avatar file
+  if (!avatarFilePath) throw new ApiError(400, "Avatar image required!!");
+
+  // Upload files cloudinary
+  const avatar = await cloudinaryUpload(avatarFilePath);
+  const coverImage = await cloudinaryUpload(coverImageFilePath);
+
+  // validate avatar file
+  if (!avatar) throw new ApiError(400, "Avatar image required!!");
+
+  // Creating user
+  const user = await User.create({
+    fullName,
+    email,
+    password,
+    username: username.toLowerCase(),
+    avatar: avatar.url,
+    coverImage: coverImage?.url || "",
   });
+
+  // Find registered User
+  const registeredUser = await User.findById(user._id).selected(
+    "-password -refreshToken"
+  );
+
+  // Validate registered user
+  if (!registeredUser) throw new ApiError(500, "Error while registering user");
+
+  // Success response
+  return res
+    .status(201)
+    .json(new ApiResponse(200, "User registered successfully", registerUser));
 });
