4fe0da00856ccfc1e72fb2ed448fbce6d3b8bdf1
Vishal Kumar Sah <thevishalkumar25@gmail.com>
2025-01-30 00:13:59 +0530

    Added login user controller

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -53,3 +53,49 @@ export const registerUser = asyncHandler(async (req, res) => {
     .status(201)
     .json(new ApiResponse(200, "User registered successfully", registeredUser));
 });
+
+export const loginUser = asyncHandler(async (req, res) => {
+  const { username, email, password } = req.body;
+
+  // Validate username and email
+  if (!username || !email)
+    throw new ApiError(400, "Username or email is required!!");
+
+  // Find and validate existing user
+  const existingUser = await User.findOne({ $or: [{ username }, { email }] });
+  if (!existingUser) throw new ApiError(404, "User does not exist");
+
+  // Validate password
+  const correctPassword = await existingUser.isCorrectPassword(password);
+  if (!correctPassword) throw new ApiError(401, "Incorrect password!!");
+
+  // Generate access and refresh token
+  const accessToken = await existingUser.generateAccessToken();
+  const refreshToken = await existingUser.generateRefreshToken();
+
+  existingUser.refreshToken = refreshToken;
+  await existingUser.save({ validateBeforeSave: false });
+  console.log("Existing User: ", existingUser);
+
+  // Send cookies
+  const loggedInUser = await User.findOne(existingUser._id).select(
+    "-password -refreshToken"
+  );
+
+  const options = {
+    httpOnly: true,
+    secure: true,
+  };
+
+  res
+    .status(200)
+    .cookie("accessToken", accessToken, options)
+    .cookie("refreshToken", refreshToken, options)
+    .json(
+      new ApiResponse(
+        200,
+        { user: loggedInUser, accessToken, refreshToken },
+        "User logged in successfully!!"
+      )
+    );
+});
